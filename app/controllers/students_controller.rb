52525011f9f35241a47a529d624b2b144dcbdb88
ivnicolas <ikennanicolas@gmail.com>
2021-04-30 21:36:32 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,7 +21,8 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.all
+    @students =  Student.where("name LIKE ?", "%M%")
+   
   end
 
   def student_params
--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,18 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+    if query 
+      name = Student.find_by(name: query)
+      if name 
+        Student.where("name LIKE ?", name)
+      else 
+        Student.all 
+      end 
+    else 
+      Student.all 
+    end 
+  end
+
 end
